649788d9a14488e49b337cb36f9cf7cec1d43fd1
palmcm <palmermonkeyboy@gmail.com>
2023-01-31 00:19:46 +0700

    feat: add jwt config and make ConfigModule global

--- apps/api/src/config/configuration.ts
+++ apps/api/src/config/configuration.ts
@@ -1,7 +1,17 @@
 export interface Configuration {
   port: number
+  jwt: {
+    secret: string
+    expires: number
+  }
 }
 
 export const configuration = () => {
-  return { port: parseInt(process.env.PORT, 10) || 4321 }
+  return {
+    port: parseInt(process.env.PORT, 10) || 4321,
+    jwt: {
+      secret: process.env.JWT_SECRET,
+      expires: process.env.JWT_EXPIRES,
+    },
+  }
 }
