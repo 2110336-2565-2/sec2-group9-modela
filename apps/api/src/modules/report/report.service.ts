06ede0808b84d28e0302200294afd41825124268
BadNameOk <jjpocharatf2@gmail.com>
2023-01-31 23:38:58 +0700

    feat: added the missing report service

--- apps/api/src/modules/report/report.service.ts
+++ apps/api/src/modules/report/report.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, InternalServerErrorException } from '@nestjs/common'
+import { ReportRepository } from 'dist/src/modules/report/report.repository'
+
+import { PostReportDTO } from './report.dto'
+import { ReportPostData } from './report.type'
 
 @Injectable()
-export class ReportService {}
+export class ReportService {
+  constructor(private readonly reportRepository: ReportRepository) {}
+
+  async postReport(id: number, postReportDTO: PostReportDTO, userId: number) {
+    const reportPostData = new ReportPostData(id, userId, postReportDTO.reason)
+    // TODO: Raise a 404 error if the job is not found
+
+    try {
+      await this.reportRepository.createCasting(reportPostData)
+    } catch (e) {
+      console.log(e)
+      throw new InternalServerErrorException()
+    }
+  }
+}
