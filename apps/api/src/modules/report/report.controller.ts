94d6a904fd2a83abef336086d421db44da6f070c
BadNameOk <jjpocharatf2@gmail.com>
2023-01-31 23:30:00 +0700

    feat: added postReport in service

--- apps/api/src/modules/report/report.controller.ts
+++ apps/api/src/modules/report/report.controller.ts
@@ -1,16 +1,29 @@
+<<<<<<< beta
 import { JobIdDTO } from '@modela/dtos'
 import { PostReportDTO } from '@modela/dtos'
 import { Body, Controller, Param, Post } from '@nestjs/common'
+=======
+import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+>>>>>>> feat: added postReport in service
 import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
   ApiForbiddenResponse,
   ApiNotFoundResponse,
+  ApiOkResponse,
   ApiOperation,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger'
 
+<<<<<<< beta
+=======
+import { JwtDto } from '../auth/auth.dto'
+import { UseAuthGuard } from '../auth/jwt.decorator'
+import { User } from '../auth/user.decorator'
+import { JobIdDTO } from '../job/job.dto'
+import { PostReportDTO } from './report.dto'
+>>>>>>> feat: added postReport in service
 import { ReportService } from './report.service'
 
 @ApiTags('report')
@@ -18,14 +31,19 @@ import { ReportService } from './report.service'
 export class ReportController {
   constructor(private readonly reportService: ReportService) {}
 
-  @Post(':id')
+  @Post('job/:id')
+  @UseAuthGuard()
   @ApiCreatedResponse({ type: JobIdDTO })
   @ApiUnauthorizedResponse({ description: 'User is not login' })
   @ApiForbiddenResponse({ description: 'User is not actor' })
   @ApiBadRequestResponse({ description: 'Wrong format' })
   @ApiNotFoundResponse({ description: 'Job not found' })
   @ApiOperation({ summary: 'write report' })
-  postReport(@Param('id') id: number, @Body() postReportDTO: PostReportDTO) {
-    return 'this will post report'
+  postReport(
+    @Param('id') id: number,
+    @Body() postReportDTO: PostReportDTO,
+    @User() user: JwtDto,
+  ) {
+    return this.reportService.postReport(id, postReportDTO, user.userId)
   }
 }
---
+++ apps/api/src/modules/report/report.repository.ts
@@ -0,0 +1,18 @@
+import { PrismaService } from 'src/database/prisma.service'
+
+import { ReportPostData } from './report.type'
+
+export class ReportRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async createCasting(payload: ReportPostData) {
+    const { jobID, userID, reason } = payload
+    await this.prisma.report.create({
+      data: {
+        jobId: jobID,
+        reporterId: userID,
+        reason: reason,
+      },
+    })
+  }
+}
