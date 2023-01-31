873edd0e51f37618abe4c70741e6a7f5d54d7a1f
Pisuttun <pisuttun@gmail.com>
2023-01-31 22:03:17 +0700

    refactor: remove hard coded background color

--- apps/web/src/common/components/Header/styled.ts
+++ apps/web/src/common/components/Header/styled.ts
@@ -11,7 +11,7 @@ export const HeaderContainer = styled('div')`
   height: 6vh;
   left: 0px;
   top: 0px;
-  background: #5b85aa;
+  background: ${({ theme }) => theme.palette.primary.main};
   box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.14), 0px 3px 3px rgba(0, 0, 0, 0.12),
     0px 1px 8px rgba(0, 0, 0, 0.2);
 `
