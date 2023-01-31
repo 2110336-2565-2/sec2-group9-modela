982594feda8fda9c37a46cb20807eb3267922772
BadNameOk <jjpocharatf2@gmail.com>
2023-01-31 23:58:19 +0700

    feat: added test module

--- apps/api/src/modules/report/report.service.spec.ts
+++ apps/api/src/modules/report/report.service.spec.ts
@@ -1,19 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaService } from 'src/database/prisma.service'
 
+import { ReportRepository } from './report.repository'
 import { ReportService } from './report.service'
 
 describe('ReportService', () => {
   let service: ReportService
+  let repository: ReportRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReportService],
+      providers: [ReportService, ReportRepository, PrismaService],
     }).compile()
 
     service = module.get<ReportService>(ReportService)
+    repository = module.get<ReportRepository>(ReportRepository)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('postReport', () => {
+    const postReportDTO = {
+      reason: 'Insert reason',
+    }
+
+    it('should post report correctly', async () => {
+      jest.spyOn(repository, 'createReport').mockResolvedValue()
+
+      await service.postReport(1, postReportDTO, 1)
+      expect(repository.createReport).toBeCalledWith(
+        expect.objectContaining(postReportDTO),
+      )
+    })
   })
 })
