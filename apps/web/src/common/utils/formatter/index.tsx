097beb5bf8e813fed2368ac55b651d3a7dbdef62
pisuttun <pisuttun@gmail.com>
2023-01-31 13:21:02 +0700

    refactor: using dayjs to format date/time

--- apps/web/src/common/utils/formatter/index.tsx
+++ apps/web/src/common/utils/formatter/index.tsx
@@ -1,14 +1,11 @@
+import dayjs from 'dayjs'
+
 //format Date to dd/mm/yy format
 export const formatDate = (date: Date): string => {
-  const day = date.getDate()
-  const month = date.getMonth() + 1
-  const year = (date.getFullYear() + 543).toString().slice(2)
-  return `${day}/${month}/${year}`
+  return dayjs(date).format('DD/MM/YY')
 }
 
 //format Date to hh:mm format
 export const formatTime = (time: Date): string => {
-  const hour = time.getHours()
-  const minute = time.getMinutes()
-  return `${hour}:${minute}`
+  return dayjs(time).format('HH:mm')
 }
