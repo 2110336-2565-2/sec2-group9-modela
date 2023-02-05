0f097c2c2a46f1f850ab602891fe021543473be4
miello <partearth1212@gmail.com>
2023-02-06 00:00:21 +0700

    feat: implement api proxy to fix set-cookie

--- apps/web/src/common/utils/api/axiosInstance/index.ts
+++ apps/web/src/common/utils/api/axiosInstance/index.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
+import { API_BASE_URL, ENABLE_PROXY, SITE_URL } from 'common/config/env'
+
+const BASE_URL = ENABLE_PROXY ? `${SITE_URL}/apiProxy` : API_BASE_URL
 
 export const apiClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  baseURL: BASE_URL,
   timeout: 10000,
   withCredentials: true,
 })
