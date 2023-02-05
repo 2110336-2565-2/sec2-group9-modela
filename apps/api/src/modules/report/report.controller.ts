bdd10e2e242cf3d59eeafb40ac9e2d8128372f94
BadNameOk <jjpocharatf2@gmail.com>
2023-02-06 00:30:31 +0700

    fix: disabled admins & casting creating report

--- apps/api/src/modules/report/report.controller.ts
+++ apps/api/src/modules/report/report.controller.ts
@@ -1,18 +1,17 @@
 import { JobIdDTO, JwtDto } from '@modela/dtos'
 import { PostReportDTO } from '@modela/dtos'
-import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { Body, Controller, Param, Post } from '@nestjs/common'
 import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
   ApiForbiddenResponse,
   ApiNotFoundResponse,
-  ApiOkResponse,
   ApiOperation,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger'
 
-import { UseAuthGuard } from '../auth/misc/jwt.decorator'
+import { UseTypeAuthGuard } from '../auth/misc/jwt.decorator'
 import { User } from '../auth/misc/user.decorator'
 import { ReportService } from './report.service'
 
@@ -22,8 +21,8 @@ export class ReportController {
   constructor(private readonly reportService: ReportService) {}
 
   @Post('job/:id')
-  @UseAuthGuard()
   @ApiCreatedResponse({ type: JobIdDTO })
+  @UseTypeAuthGuard('ACTOR')
   @ApiUnauthorizedResponse({ description: 'User is not login' })
   @ApiForbiddenResponse({ description: 'User is not actor' })
   @ApiBadRequestResponse({ description: 'Wrong format' })
