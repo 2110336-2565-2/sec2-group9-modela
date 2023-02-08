7b8f8c97e5c92dfb5dffedd9dd56b53d8eb5b04f
Pisuttun <pisuttun@gmail.com>
2023-02-08 21:37:48 +0700

    fix: number validation

--- apps/web/src/modules/job/post/hooks/useJobForm/schema.ts
+++ apps/web/src/modules/job/post/hooks/useJobForm/schema.ts
@@ -5,7 +5,11 @@ import { z } from 'zod'
 const shootingSchema = z
   .object({
     location: z.string({ required_error: 'กรุณากรอกสถานที่ถ่ายทำ' }),
-    startDate: z.instanceof(dayjs as unknown as typeof Dayjs),
+    startDate: z
+      .instanceof(dayjs as unknown as typeof Dayjs)
+      .refine((arg) => arg.isAfter(dayjs()), {
+        message: 'วันที่เริ่มต้นการถ่ายทำต้องมากกว่าวันที่ปัจจุบัน',
+      }),
     endDate: z.instanceof(dayjs as unknown as typeof Dayjs),
     startTime: z.instanceof(dayjs as unknown as typeof Dayjs),
     endTime: z.instanceof(dayjs as unknown as typeof Dayjs),
@@ -33,17 +37,37 @@ const postJobSchema = z
   .object({
     jobName: z.string({ required_error: 'กรุณากรอกตำแหน่งงาน' }),
     jobDescription: z.string({ required_error: 'กรุณากรอกรายละเอียดงาน' }),
-    dueDate: z.instanceof(dayjs as unknown as typeof Dayjs),
-    wage: z.string({ required_error: 'กรุณากรอกค่าจ้าง' }),
-    actorCount: z.string({ required_error: 'กรุณากรอกจำนวนนักแสดง' }),
+    dueDate: z
+      .instanceof(dayjs as unknown as typeof Dayjs)
+      .refine((arg) => arg.isAfter(dayjs()), {
+        message: 'วันที่สิ้นสุดการรับสมัครต้องมากกว่าวันที่ปัจจุบัน',
+      }),
+    wage: z
+      .string({ required_error: 'กรุณากรอกค่าจ้าง' })
+      .refine((arg) => /^[0-9]*\.?[0-9]*$/.test(arg), {
+        message: 'กรุณาค่าจ้างกรอกเป็นตัวเลข',
+      }),
+    actorCount: z
+      .string({ required_error: 'กรุณากรอกจำนวนนักแสดง' })
+      .refine((arg) => /^[0-9]*$/.test(arg), {
+        message: 'กรุณากรอกจำนวนนักแสดงเป็นตัวเลขจำนวนเต็ม',
+      }),
     gender: z.nativeEnum(Gender),
-    minAge: z.string({ required_error: 'กรุณากรอกอายุต่ำสุด' }),
-    maxAge: z.string({ required_error: 'กรุณากรอกอายุสูงสุด' }),
+    minAge: z
+      .string({ required_error: 'กรุณากรอกอายุต่ำสุด' })
+      .refine((arg) => /^[0-9]*$/.test(arg), {
+        message: 'กรุณากรอกอายุต่ำสุดเป็นตัวเลขจำนวนเต็ม',
+      }),
+    maxAge: z
+      .string({ required_error: 'กรุณากรอกอายุสูงสุด' })
+      .refine((arg) => /^[0-9]*$/.test(arg), {
+        message: 'กรุณากรอกอายุสูงสุดเป็นตัวเลขจำนวนเต็ม',
+      }),
     role: z.string({ required_error: 'กรุณากรอกบทบาท' }),
     shooting: z.array(shootingSchema),
   })
   .superRefine(({ minAge, maxAge }, ctx) => {
-    if (minAge && maxAge && minAge > maxAge) {
+    if (minAge && maxAge && +minAge > +maxAge) {
       ctx.addIssue({
         code: 'custom',
         path: ['maxAge'],
