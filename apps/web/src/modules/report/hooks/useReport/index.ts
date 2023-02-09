d16cfe043f2897dcb43c5fc6416a3d37a72504c8
Pisuttun <pisuttun@gmail.com>
2023-02-09 17:41:22 +0700

    feat: connect to report API

--- apps/web/src/modules/report/hooks/useReport/index.ts
+++ apps/web/src/modules/report/hooks/useReport/index.ts
@@ -2,15 +2,15 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { GetJobDto } from '@modela/dtos'
 import { apiClient } from 'common/utils/api'
 import { useRouter } from 'next/router'
-import { FormEventHandler, useEffect, useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { FormEventHandler, useCallback, useEffect, useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { IReportSchemaType, ReportSchema } from './schema'
 
 const useReport = () => {
   const router = useRouter()
   const { jid } = router.query
-  const [jobName, setJobName] = useState<string>('')
+  const [jobName, setJobName] = useState('')
   const { control, handleSubmit } = useForm<IReportSchemaType>({
     criteriaMode: 'all',
     resolver: zodResolver(ReportSchema),
@@ -18,12 +18,26 @@ const useReport = () => {
       description: '',
     },
   })
-  const handleSuccess = () => {
-    console.log('success')
-  }
+
+  const handleSuccess: SubmitHandler<IReportSchemaType> = useCallback(
+    async ({ ...data }) => {
+      try {
+        const postBody = {
+          reason: data.description,
+        }
+        await apiClient.post('report/job/' + jid, postBody)
+        router.push('/job')
+      } catch (err) {
+        console.log(err)
+      }
+    },
+    [router],
+  )
+
   const handleClickSubmit: FormEventHandler<HTMLFormElement> =
     handleSubmit(handleSuccess)
 
+  //fetch job title
   useEffect(() => {
     const fetchData = async () => {
       try {
