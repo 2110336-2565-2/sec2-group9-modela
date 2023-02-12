36162d4e63efdea4800d7b72af3f06b3d0b06ab8
Pisuttun <pisuttun@gmail.com>
2023-02-12 23:12:07 +0700

    fix: fix test file

--- apps/web/src/modules/job/components/JobCardHeader/index.test.tsx
+++ apps/web/src/modules/job/components/JobCardHeader/index.test.tsx
@@ -1,20 +1,27 @@
-import { mock, UserType } from '@modela/dtos'
+import { JobStatus, mock, UserType } from '@modela/dtos'
 import { render } from '@testing-library/react'
 import { mockUser } from 'common/utils/testing'
 import React from 'react'
 
-describe('<Header />', () => {
+describe('<JobCardHeader />', () => {
   const MOCK_TITLE = mock('job').get().title
   const MOCK_COMPANY_NAME = mock('casting').get().companyName
   const MOCK_CASTING_IMAGE = mock('user').get().profileImageUrl || ''
+  const MOCK_STATUS = JobStatus.OPEN
   const headerProps = {
     castingImage: MOCK_CASTING_IMAGE,
     companyName: MOCK_COMPANY_NAME,
     title: MOCK_TITLE,
+    status: MOCK_STATUS,
   }
   const { mockUserType } = mockUser()
 
-  const { default: Header } = require('.') as typeof import('.')
+  jest.mock('next/router', () => ({
+    useRouter: jest.fn(),
+  }))
+  const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+
+  const { default: JobCardHeader } = require('.') as typeof import('.')
 
   afterEach(() => {
     jest.clearAllMocks()
@@ -23,20 +30,24 @@ describe('<Header />', () => {
   describe('normal behavior', () => {
     it('should render Header correctly', () => {
       mockUserType(UserType.CASTING)
-      const { getByText } = render(<Header {...headerProps} />)
+      useRouter.mockImplementation(() => ({
+        query: { jobId: 1 },
+        isReady: true,
+      }))
+      const { getByText } = render(<JobCardHeader {...headerProps} />)
       expect(getByText(MOCK_TITLE)).toBeDefined()
       expect(getByText(MOCK_COMPANY_NAME)).toBeDefined()
     })
 
     it('should render correctly when user is Actor', () => {
       mockUserType(UserType.ACTOR)
-      const { getByText } = render(<Header {...headerProps} />)
+      useRouter.mockImplementation(() => ({
+        query: { jobId: 1 },
+        isReady: true,
+      }))
+      const { getByText } = render(<JobCardHeader {...headerProps} />)
       expect(getByText(MOCK_TITLE)).toBeDefined()
       expect(getByText(MOCK_COMPANY_NAME)).toBeDefined()
-    })
-
-    it('should call report function correctly', () => {
-      // TODO: write this test after implement report function properly
     })
   })
 })
