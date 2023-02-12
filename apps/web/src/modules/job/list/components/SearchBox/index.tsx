85ce70ee997f8bba1eb3a1e38dd9c70a19b18303
Naphat <priasdevo@gmail.com>
2023-02-12 22:10:33 +0700

    feat: wrap searchBox with form to search when enter is press inside search box

--- apps/web/src/modules/job/list/components/SearchBox/index.tsx
+++ apps/web/src/modules/job/list/components/SearchBox/index.tsx
@@ -7,23 +7,31 @@ import { SearchBoxProps } from './types'
 export default function SearchBox(prop: SearchBoxProps) {
   const { filterData, state, setState, labels } = prop
   return (
-    <TextField
-      placeholder={labels}
-      value={state.title}
-      onChange={(event) => {
-        setState({ ...state, title: event.target.value })
+    <form
+      onSubmit={(event) => {
+        event.preventDefault()
+        filterData(state)
       }}
-      InputProps={{
-        endAdornment: (
-          <SearchOutlinedIcon
-            color="primary"
-            sx={{ cursor: 'pointer' }}
-            onClick={() => {
-              filterData(state)
-            }}
-          />
-        ),
-      }}
-    />
+    >
+      <TextField
+        placeholder={labels}
+        fullWidth={true}
+        value={state.title}
+        onChange={(event) => {
+          setState({ ...state, title: event.target.value })
+        }}
+        InputProps={{
+          endAdornment: (
+            <SearchOutlinedIcon
+              color="primary"
+              sx={{ cursor: 'pointer' }}
+              onClick={() => {
+                filterData(state)
+              }}
+            />
+          ),
+        }}
+      />
+    </form>
   )
 }
