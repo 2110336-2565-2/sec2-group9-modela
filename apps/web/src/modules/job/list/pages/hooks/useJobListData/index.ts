7f3f2478d85873a09b39dd2ed9160f8ee9b50c95
Naphat <priasdevo@gmail.com>
2023-02-12 21:18:33 +0700

    fix: useCallback wrap all the function

--- apps/web/src/modules/job/list/pages/hooks/useJobListData/index.ts
+++ apps/web/src/modules/job/list/pages/hooks/useJobListData/index.ts
@@ -2,7 +2,7 @@ import { GetJobCardWithMaxPageDto } from '@modela/dtos'
 import { apiClient } from 'common/utils/api'
 import dayjs from 'dayjs'
 import { useRouter } from 'next/router'
-import { useRef, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 
 import { IFilter, initialISearch, ISearch } from '../../types'
 
@@ -13,48 +13,55 @@ const useJobListData = () => {
   const [page, setPage] = useState(0)
   const pageControl = useRef(1)
   const [search, setSearch] = useState<ISearch>(initialISearch)
-  const filterData = async (state: IFilter) => {
-    let newStatus = []
-    let newGender = []
+  const filterData = useCallback(
+    async (state: IFilter) => {
+      let newStatus = []
+      let newGender = []
 
-    if (state.openCheck) {
-      newStatus.push('OPEN')
-    }
-    if (state.closeCheck) {
-      newStatus.push('CLOSE')
-    }
-    if (state.maleCheck) {
-      newGender.push('MALE')
-    }
-    if (state.femaleCheck) {
-      newGender.push('FEMALE')
-    }
-    if (state.otherCheck) {
-      newGender.push('OTHER')
-    }
+      if (state.openCheck) {
+        newStatus.push('OPEN')
+      }
+      if (state.closeCheck) {
+        newStatus.push('CLOSE')
+      }
+      if (state.maleCheck) {
+        newGender.push('MALE')
+      }
+      if (state.femaleCheck) {
+        newGender.push('FEMALE')
+      }
+      if (state.otherCheck) {
+        newGender.push('OTHER')
+      }
 
-    setSearch({
-      ...search,
-      title: state.title,
-      age: state.age,
-      startDate: state.startDate ? dayjs(state.startDate).toISOString() : null,
-      endDate: state.endDate ? dayjs(state.endDate).toISOString() : null,
-      location: state.location,
-      startTime: state.startTime ? dayjs(state.startTime).toISOString() : null,
-      endTime: state.endTime ? dayjs(state.endTime).toISOString() : null,
-      minWage: Number(state.wage) - Number(state.deviant),
-      maxWage: Number(state.wage) + Number(state.deviant),
-      status: newStatus,
-      gender: newGender,
-    })
+      setSearch({
+        ...search,
+        title: state.title,
+        age: state.age,
+        startDate: state.startDate
+          ? dayjs(state.startDate).toISOString()
+          : null,
+        endDate: state.endDate ? dayjs(state.endDate).toISOString() : null,
+        location: state.location,
+        startTime: state.startTime
+          ? dayjs(state.startTime).toISOString()
+          : null,
+        endTime: state.endTime ? dayjs(state.endTime).toISOString() : null,
+        minWage: Number(state.wage) - Number(state.deviant),
+        maxWage: Number(state.wage) + Number(state.deviant),
+        status: newStatus,
+        gender: newGender,
+      })
 
-    setJob({ ...job, jobs: [], maxPage: 1 })
-    setPage(1)
-    pageControl.current = 0
+      setJob({ ...job, jobs: [], maxPage: 1 })
+      setPage(1)
+      pageControl.current = 0
 
-    setHasMore(true)
-  }
-  const fetchData = async () => {
+      setHasMore(true)
+    },
+    [search, job, setJob, setPage, pageControl, setHasMore],
+  )
+  const fetchData = useCallback(async () => {
     try {
       if (pageControl.current <= page) {
         pageControl.current = page + 1
@@ -79,7 +86,7 @@ const useJobListData = () => {
       console.log(e)
       router.replace('/404')
     }
-  }
+  }, [apiClient, page, search, setHasMore, setJob, setPage])
   return { job, hasMore, fetchData, filterData }
 }
 
