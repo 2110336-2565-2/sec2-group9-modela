5fd395e03e6729ea64ac86cc3625e9f8c028d799
miello <partearth1212@gmail.com>
2023-02-12 22:53:24 +0700

    fix: failed useRouter

--- apps/web/src/modules/signup/choose/index.test.tsx
+++ apps/web/src/modules/signup/choose/index.test.tsx
@@ -1,10 +1,17 @@
 import { render, waitFor } from '@testing-library/react'
-import { mockAndSpy } from 'common/utils/testing'
+import { mockAndSpy, mockAndSpyMany } from 'common/utils/testing'
 
 describe('<ChooseSignup />', () => {
   const LinkMock = mockAndSpy('next/link')
   const ImageSpy = mockAndSpy('next/image')
   ImageSpy.mockImplementation(() => {})
+
+  const replaceSpy = jest.fn()
+  const useRouterSpy = jest.fn(() => ({ replace: replaceSpy }))
+  jest.doMock('next/router', () => ({ useRouter: useRouterSpy }))
+
+  const [mediaQuerySpy] = mockAndSpyMany('@mui/material', ['useMediaQuery'])
+  mediaQuerySpy.mockImplementation(() => true)
 
   const { default: ChooseSignup } = require('.') as typeof import('.')
   const HREF_TARGET: Record<string, string> = {
