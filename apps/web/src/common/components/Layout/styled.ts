e6be5f05fd3336bd2241f825162f4576a54c11e6
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-02-15 21:47:38 +0700

    fix: layout height bug

--- apps/web/src/common/components/Layout/styled.ts
+++ apps/web/src/common/components/Layout/styled.ts
@@ -6,8 +6,5 @@ export const PageContainer = styled('div')`
   align-items: center;
   flex-direction: column;
   background-color: #f5f5f5;
-  min-height: 100vh;
-  min-height: -moz-available;
-  min-height: -webkit-fill-available;
-  min-height: fill-available;
+  min-height: 100dvh;
 `
