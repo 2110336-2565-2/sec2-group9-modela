cc20e29bc26ae6d554d415a829db1930c0567c58
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-02-15 23:07:53 +0700

    fix: loading overflow (3) mai wai laew ;w;

--- apps/web/src/common/context/UserContext/index.tsx
+++ apps/web/src/common/context/UserContext/index.tsx
@@ -1,4 +1,5 @@
 import { CircularProgress } from '@mui/material'
+import { PageContainer } from 'common/components/Layout/styled'
 import React, { createContext } from 'react'
 
 import useUserData from './hooks/useUserData'
@@ -11,7 +12,12 @@ export const useUser = () => React.useContext(UserContext)
 export const UserProvider = ({ children }: React.PropsWithChildren<{}>) => {
   const { user, isLoading, refetch, reset } = useUserData()
 
-  if (isLoading) return <CircularProgress sx={{ alignSelf: 'center' }} />
+  if (isLoading)
+    return (
+      <PageContainer>
+        <CircularProgress />
+      </PageContainer>
+    )
 
   return (
     <UserContext.Provider value={{ user, refetch, reset }}>
