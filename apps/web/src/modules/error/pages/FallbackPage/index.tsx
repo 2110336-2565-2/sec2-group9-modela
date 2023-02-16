d562ebb839ea82844ab009ded82086ccb4743da4
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-02-16 22:26:03 +0700

    refactor: edit fallback page style

--- apps/web/src/modules/error/pages/FallbackPage/index.tsx
+++ apps/web/src/modules/error/pages/FallbackPage/index.tsx
@@ -2,25 +2,17 @@ import { SentimentVeryDissatisfiedOutlined } from '@mui/icons-material'
 import { Button, Theme, Typography, useMediaQuery } from '@mui/material'
 import React from 'react'
 
+import { FallbackContainer } from './styled'
+
 const FallbackPage = () => {
   const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'))
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        textAlign: 'center',
-        padding: '20px',
-        gap: '10px',
-      }}
-    >
+    <FallbackContainer>
       <Typography variant={isMobile ? 'h6' : 'h4'}>
         ขออภัย เราไม่พบหน้าที่คุณต้องการค้นหา
         <SentimentVeryDissatisfiedOutlined
-          style={{ transform: 'translateY(2px)', marginLeft: '5px' }}
+          sx={{ transform: 'translateY(2px)', marginLeft: '5px' }}
         />
       </Typography>
       <Typography variant={isMobile ? 'subtitle1' : 'h6'} color="#0000009A">
@@ -29,7 +21,7 @@ const FallbackPage = () => {
       <Button href="/" size="large" sx={{ marginTop: '50px' }}>
         กลับหน้าหลัก
       </Button>
-    </div>
+    </FallbackContainer>
   )
 }
 
