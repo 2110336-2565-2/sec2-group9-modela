e9e2f515c2c7433b8fcab742fdb25aeb7079cce9
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-02-17 10:32:27 +0700

    test: add profile image test

---
+++ apps/web/src/common/components/ProfileImage/index.test.tsx
@@ -0,0 +1,38 @@
+import { render } from '@testing-library/react'
+import React from 'react'
+
+describe('<ProfileImage />', () => {
+  const defaultProps = {
+    src: 'https://picsum.photos/200',
+    userId: 1,
+    firstName: 'John',
+  }
+
+  const { default: ProfileImage } = require('.') as typeof import('.')
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  describe('normal behavior', () => {
+    describe('display', () => {
+      it('should render ProfileImage correctly', () => {
+        render(<ProfileImage {...defaultProps} />)
+
+        // TODO: expect image to be rendered ater changed to next image
+      })
+    })
+  })
+
+  describe('src is undefined', () => {
+    describe('display', () => {
+      it('should render fallback image correctly', () => {
+        const { getByText } = render(
+          <ProfileImage {...defaultProps} src={undefined} />,
+        )
+
+        expect(getByText('J')).toBeDefined()
+      })
+    })
+  })
+})
