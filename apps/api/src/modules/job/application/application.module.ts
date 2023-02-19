f44536df76b32baa1bed2e84cfc41c4e112e2ac7
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-02-19 13:46:14 +0700

    fix: change imports

--- apps/api/src/modules/job/application/application.module.ts
+++ apps/api/src/modules/job/application/application.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common'
 import { PrismaModule } from 'src/database/prisma.module'
 
-import { JobRepository } from '../job.repository'
+import { JobModule } from '../job.module'
 import { ApplicationController } from './application.controller'
 import { ApplicationRepository } from './application.repository'
 import { ApplicationService } from './application.service'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, JobModule],
   controllers: [ApplicationController],
-  providers: [ApplicationService, ApplicationRepository, JobRepository],
+  providers: [ApplicationService, ApplicationRepository],
   exports: [ApplicationRepository],
 })
 export class ApplicationModule {}
--- apps/api/src/modules/job/job.module.ts
+++ apps/api/src/modules/job/job.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common'
 import { PrismaModule } from 'src/database/prisma.module'
 
-import { ApplicationModule } from './application/application.module'
 import { JobController } from './job.controller'
 import { JobRepository } from './job.repository'
 import { JobService } from './job.service'
 
 @Module({
-  imports: [PrismaModule, ApplicationModule],
+  imports: [PrismaModule],
   controllers: [JobController],
   providers: [JobService, JobRepository],
   exports: [JobRepository],
