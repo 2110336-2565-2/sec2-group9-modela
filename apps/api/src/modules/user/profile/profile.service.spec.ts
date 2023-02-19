7a6a27fabd219f8653468d3b3f91e6082881dfbb
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-02-19 20:43:09 +0700

    test: add update actor profile test

--- apps/api/src/modules/user/profile/profile.service.spec.ts
+++ apps/api/src/modules/user/profile/profile.service.spec.ts
@@ -1,19 +1,55 @@
+import { EditActorProfileDto, mock } from '@modela/dtos'
 import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaService } from 'src/database/prisma.service'
 
+import { ProfileRepository } from './profile.repository'
 import { ProfileService } from './profile.service'
 
 describe('ProfileService', () => {
   let service: ProfileService
+  let repository: ProfileRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProfileService],
+      providers: [ProfileService, ProfileRepository, PrismaService],
     }).compile()
 
     service = module.get<ProfileService>(ProfileService)
+    repository = module.get<ProfileRepository>(ProfileRepository)
   })
 
   it('should be defined', () => {
     expect(service).toBeDefined()
+  })
+
+  describe('updateActor', () => {
+    it('should update actor profile correctly', () => {
+      const MOCK_USER_ID = 1
+      const payload: EditActorProfileDto = {
+        ...mock('actor')
+          .omit([
+            'actorId',
+            'idCardImageUrl',
+            'prefix',
+            'nationality',
+            'ssn',
+            'gender',
+          ])
+          .get(),
+        ...mock('user')
+          .pick([
+            'profileImageUrl',
+            'phoneNumber',
+            'description',
+            'bankAccount',
+            'bankName',
+          ])
+          .get(),
+      }
+
+      jest.spyOn(repository, 'updateActor').mockResolvedValue()
+      service.updateActor(MOCK_USER_ID, payload)
+      expect(repository.updateActor).toBeCalledWith(MOCK_USER_ID, payload)
+    })
   })
 })
