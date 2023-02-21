c98497e3d0336bdfa04d99e66eab4a178e5b63de
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-02-21 14:11:27 +0700

    feat: add job menu to detail page

--- apps/web/src/modules/job/detail/pages/styled.ts
+++ apps/web/src/modules/job/detail/pages/styled.ts
@@ -1,9 +1,23 @@
 import { styled } from '@mui/material'
 
-export const RootContainer = styled('div')`
+export const PageContainer = styled('form')`
   display: flex;
+  margin: 50px;
+  width: 100%;
+  gap: 64px;
+  align-items: flex-start;
   justify-content: center;
-  padding-top: 3rem;
+
+  ${(props) => props.theme.breakpoints.down('md')} {
+    margin: 40px 30px;
+  }
+
+  ${(props) => props.theme.breakpoints.down('sm')} {
+    margin: 25px 15px;
+  }
+`
+
+export const JobCardContainer = styled('div')`
   width: 50vw;
   ${(props) => props.theme.breakpoints.down('md')} {
     width: 90vw;
