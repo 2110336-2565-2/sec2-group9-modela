917067fab55a8e25f1d3668ce2c9be21b0141e82
BadNameOk <jjpocharatf2@gmail.com>
2023-02-21 17:35:00 +0700

    feat: added Dto for report

--- packages/dtos/src/report.ts
+++ packages/dtos/src/report.ts
@@ -1,8 +1,35 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsString } from 'class-validator'
+import { ApiProperty, OmitType } from '@nestjs/swagger'
+import { IsInt, IsNotEmpty, IsString } from 'class-validator'
 
-export class PostReportDto {
+export class ReportDto{
+
+  @IsInt()
+  @IsNotEmpty()
+  @ApiProperty()
+  reportId: number
+
+  @IsInt()
+  @IsNotEmpty()
+  @ApiProperty()
+  reporterId: number  
+
+
   @ApiProperty()
   @IsString()
   reason: string
+
+}
+
+export class PostReportDto extends OmitType(ReportDto, ['reportId', 'reporterId']) {}
+
+export class GetReportDto extends ReportDto {}
+
+export class GetReportsDto{
+  @ApiProperty({ type: GetReportDto, isArray: true })
+  reports: GetReportDto[]
+
+  @IsInt()
+  @IsNotEmpty()
+  @ApiProperty()
+  jobId: number
 }
