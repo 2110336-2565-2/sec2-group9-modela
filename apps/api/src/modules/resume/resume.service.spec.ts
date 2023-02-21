1c667fba2daefe92f5106c06ac4dee2c722b36f9
BadNameOk <jjpocharatf2@gmail.com>
2023-02-21 20:12:26 +0700

    feat: unit test for POST resume

--- apps/api/src/modules/resume/resume.service.spec.ts
+++ apps/api/src/modules/resume/resume.service.spec.ts
@@ -1,8 +1,48 @@
-/*import { ResumeService } from "./resume.service";
+import { UserStatus, UserType } from '@modela/database'
+import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaService } from 'src/database/prisma.service'
 
+import { ResumeRepository } from './resume.respository'
+import { ResumeService } from './resume.service'
 
 describe('ResumeService', () => {
-    let service: ResumeService;
-})*/
+  let service: ResumeService
+  let repository: ResumeRepository
 
-export class ResumeTestUnit {}
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ResumeService, ResumeRepository, PrismaService],
+    }).compile()
+
+    service = module.get<ResumeService>(ResumeService)
+    repository = module.get<ResumeRepository>(ResumeRepository)
+  })
+
+  it('should be defined', () => {
+    expect(service).toBeDefined()
+    expect(repository).toBeDefined()
+  })
+
+  describe('postResume', () => {
+    const MOCK_RESUME_NAME = 'Test Resume'
+    const MOCK_RESUME_URL = 'github.com'
+
+    const MOCK_ACTOR = {
+      userId: 1,
+      status: UserStatus.ACCEPTED,
+      type: UserType.ACTOR,
+    }
+
+    const postResumeDto = {
+      name: MOCK_RESUME_NAME,
+      resumeUrl: MOCK_RESUME_URL,
+    }
+
+    it('should post resume correctly', async () => {
+      jest.spyOn(repository, 'createResume').mockResolvedValue({ resumeId: 11 })
+      const result = await service.createResume(postResumeDto, MOCK_ACTOR)
+      expect(repository.createResume).toBeCalledWith(postResumeDto, MOCK_ACTOR)
+      expect(result).toEqual({ resumeId: 11 })
+    })
+  })
+})
