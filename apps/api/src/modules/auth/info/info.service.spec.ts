cf932cf249f90aef639a1f573d9111ed95aea84a
palmcm <palmermonkeyboy@gmail.com>
2023-02-23 00:48:39 +0700

    fix: add service to test info module

--- apps/api/src/modules/auth/info/info.service.spec.ts
+++ apps/api/src/modules/auth/info/info.service.spec.ts
@@ -1,5 +1,9 @@
+import { ConfigService } from '@nestjs/config'
 import { Test, TestingModule } from '@nestjs/testing'
+import { PrismaService } from 'src/database/prisma.service'
+import { FileService } from 'src/modules/file/file.service'
 
+import { InfoRepository } from './info.repository'
 import { InfoService } from './info.service'
 
 describe('InfoService', () => {
@@ -7,7 +11,13 @@ describe('InfoService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [InfoService],
+      providers: [
+        InfoService,
+        PrismaService,
+        FileService,
+        InfoRepository,
+        ConfigService,
+      ],
     }).compile()
 
     service = module.get<InfoService>(InfoService)
