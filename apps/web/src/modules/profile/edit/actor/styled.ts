10acf33f11a2f5170e6e6eea8f5930410370e313
Pisuttun <pisuttun@gmail.com>
2023-02-24 15:56:04 +0700

    feat: add edit actor page draft

---
+++ apps/web/src/modules/profile/edit/actor/styled.ts
@@ -0,0 +1,19 @@
+import { Card, styled } from '@mui/material'
+
+export const CardContainer = styled(Card)`
+  display: flex;
+  justify-content: center;
+  height: fit-content;
+  padding: 16px;
+  width: 100%;
+  max-width: 500px;
+  min-height: 300px;
+  border-radius: 10px;
+`
+
+export const RootContainer = styled('div')`
+  display: flex;
+  align-items: flex-start;
+  gap: 30px;
+  margin: 20px;
+`
