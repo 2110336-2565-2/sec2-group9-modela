c0573cef5ff895972ece8bfb00188259c239da21
Pimrak Apiratchatanont <apimrak@gmail.com>
2023-02-24 22:52:56 +0700

    style: fix actor view style

--- apps/web/src/modules/job/components/JobMenu/styled.ts
+++ apps/web/src/modules/job/components/JobMenu/styled.ts
@@ -4,11 +4,12 @@ export const MenuContainer = styled('div')`
   display: flex;
   flex-direction: column;
   gap: 10px;
+  margin-bottom: -10px;
 `
 
 export const SummaryContainer = styled('div')`
   ${(props) => props.theme.breakpoints.down('lg')} {
-    margin: 10px 30px;
+    margin: 10px 30px 0 30px;
   }
 
   ${(props) => props.theme.breakpoints.down('sm')} {
