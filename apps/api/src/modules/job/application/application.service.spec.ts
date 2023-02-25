4f7e8b2ff16c28b423ab1e7a40a54ae97f4f4f89
palmcm <palmermonkeyboy@gmail.com>
2023-02-25 22:38:55 +0700

    test: add unit test for applyJob

--- apps/api/src/modules/job/application/application.service.spec.ts
+++ apps/api/src/modules/job/application/application.service.spec.ts
@@ -1,7 +1,13 @@
 import { ApplicationStatus, mock } from '@modela/database'
-import { ForbiddenException, NotFoundException } from '@nestjs/common'
+import {
+  BadRequestException,
+  ConflictException,
+  ForbiddenException,
+  NotFoundException,
+} from '@nestjs/common'
 import { Test, TestingModule } from '@nestjs/testing'
 import { PrismaService } from 'src/database/prisma.service'
+import { ResumeRepository } from 'src/modules/resume/resume.respository'
 
 import { JobRepository } from '../job.repository'
 import { ApplicationRepository } from './application.repository'
@@ -11,6 +17,7 @@ describe('ApplicationService', () => {
   let service: ApplicationService
   let repository: ApplicationRepository
   let jobRepository: JobRepository
+  let resumeRepository: ResumeRepository
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,12 +26,14 @@ describe('ApplicationService', () => {
         JobRepository,
         PrismaService,
         ApplicationRepository,
+        ResumeRepository,
       ],
     }).compile()
 
     service = module.get<ApplicationService>(ApplicationService)
     repository = module.get<ApplicationRepository>(ApplicationRepository)
     jobRepository = module.get<JobRepository>(JobRepository)
+    resumeRepository = module.get<ResumeRepository>(ResumeRepository)
   })
 
   it('should be defined', () => {
@@ -79,6 +88,74 @@ describe('ApplicationService', () => {
       expect(
         service.findByJobId(MOCK_JOB_ID, MOCK_CASTING_ID, {}),
       ).rejects.toThrow(ForbiddenException)
+    })
+  })
+
+  describe('applyJob', () => {
+    const MOCK_JOB_ID = 1
+    const MOCK_ACTORS_ID = 2
+    const MOCK_RESUME = mock('resume')
+      .override({ actorId: MOCK_ACTORS_ID })
+      .get()
+
+    const MOCK_APPLICATION = mock('application')
+      .override({
+        resumeId: MOCK_RESUME.resumeId,
+        actorId: MOCK_ACTORS_ID,
+        jobId: MOCK_JOB_ID,
+      })
+      .get()
+
+    beforeEach(() => {
+      jest
+        .spyOn(repository, 'createApplication')
+        .mockResolvedValue(MOCK_APPLICATION)
+      jest
+        .spyOn(jobRepository, 'getBaseJobById')
+        .mockResolvedValue(mock('job').get())
+      jest
+        .spyOn(resumeRepository, 'getResumeById')
+        .mockResolvedValue(MOCK_RESUME)
+      jest.spyOn(repository, 'getApplicationbyActorJob').mockResolvedValue(null)
+    })
+
+    it('should apply by job id correctly', async () => {
+      expect(
+        service.applyJob(MOCK_JOB_ID, MOCK_RESUME.resumeId, MOCK_ACTORS_ID),
+      ).resolves.toEqual({ applicationId: MOCK_APPLICATION.applicationId })
+    })
+
+    it('should throw not found error if job not found', async () => {
+      jest.spyOn(jobRepository, 'getBaseJobById').mockResolvedValue(null)
+      expect(
+        service.applyJob(MOCK_JOB_ID, MOCK_RESUME.resumeId, MOCK_ACTORS_ID),
+      ).rejects.toThrow(NotFoundException)
+    })
+
+    it('should throw bad request error if resume not found', async () => {
+      jest.spyOn(resumeRepository, 'getResumeById').mockResolvedValue(null)
+      expect(
+        service.applyJob(MOCK_JOB_ID, MOCK_RESUME.resumeId, MOCK_ACTORS_ID),
+      ).rejects.toThrow(BadRequestException)
+    })
+    it('should throw bad request error if user not the owner of resume', async () => {
+      jest.spyOn(resumeRepository, 'getResumeById').mockResolvedValue(
+        mock('resume')
+          .override({ actorId: MOCK_ACTORS_ID + 1 })
+          .get(),
+      )
+      expect(
+        service.applyJob(MOCK_JOB_ID, MOCK_RESUME.resumeId, MOCK_ACTORS_ID),
+      ).rejects.toThrow(BadRequestException)
+    })
+
+    it('should throw conflict error if user already applied job', async () => {
+      jest
+        .spyOn(repository, 'getApplicationbyActorJob')
+        .mockResolvedValue(MOCK_APPLICATION)
+      expect(
+        service.applyJob(MOCK_JOB_ID, MOCK_RESUME.resumeId, MOCK_ACTORS_ID),
+      ).rejects.toThrow(ConflictException)
     })
   })
 })
