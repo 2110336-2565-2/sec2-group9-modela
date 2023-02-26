eae2c8aeb9d6bac92018c37589a6957eb7280df9
Naphat <priasdevo@gmail.com>
2023-02-26 14:34:14 +0700

    feat: init hooks file, add some components (not done)

---
+++ apps/web/src/modules/job/appliedJob/components/JobCardContainer/index.tsx
@@ -0,0 +1,43 @@
+import { GetJobCardWithMaxPageDto } from '@modela/dtos'
+import { Typography } from '@mui/material'
+import React from 'react'
+
+import JobCard from './components/JobCard'
+import { CardBoxContainer } from './styled'
+
+export default function JobCardContainer(prop: GetJobCardWithMaxPageDto) {
+  const { jobs, maxPage } = prop
+  return (
+    <CardBoxContainer>
+      {maxPage === 0 && (
+        <Typography
+          variant="subtitle1"
+          sx={{
+            width: '100%',
+            textAlign: 'center',
+            color: 'rgba(0, 0, 0, 0.38)',
+            paddingBottom: '12px',
+          }}
+        >
+          {'ไม่พบงานที่คุณต้องการหา'}
+        </Typography>
+      )}
+      {jobs.map((item) => {
+        return (
+          <div
+            key={`Job-${item.jobId}`}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'flex-start',
+              width: '100%',
+              gap: '1rem',
+            }}
+          >
+            <JobCard {...item} />
+          </div>
+        )
+      })}
+    </CardBoxContainer>
+  )
+}
