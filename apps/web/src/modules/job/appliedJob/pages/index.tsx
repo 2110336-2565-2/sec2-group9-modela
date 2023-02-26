91fc61bb6c7aac6048a7b1a2c960b5dd71baabe8
Naphat <priasdevo@gmail.com>
2023-02-26 15:42:27 +0700

    feat: appliedJob filter container

--- apps/web/src/modules/job/appliedJob/pages/index.tsx
+++ apps/web/src/modules/job/appliedJob/pages/index.tsx
@@ -1,7 +1,119 @@
+import { CircularProgress } from '@mui/material'
 import withGuard from 'common/hoc/withGuard'
+import useNavbarSearch from 'common/hooks/useNavbarSearch'
+import FilterMobileContainer from 'modules/job/appliedJob/components/FilterMobileContainer'
+import SearchBox from 'modules/job/appliedJob/components/SearchBox'
+import { useCallback } from 'react'
+import InfiniteScroll from 'react-infinite-scroller'
+
+import FilterContainer from '../components/FilterContainer'
+import JobCardContainer from '../components/JobCardContainer'
+import useJobListData from './hooks/useJobListData'
+import {
+  FilterBoxContainer,
+  JobContainer,
+  PlaceFill,
+  SearchContainer,
+} from './styled'
 
 const AppliedJobPage = () => {
-  return <div></div>
+  const {
+    job,
+    hasMore,
+    fetchData,
+    filterData,
+    state,
+    setState,
+    isOpen,
+    open,
+    close,
+  } = useJobListData()
+  useNavbarSearch(
+    useCallback(() => {
+      open()
+    }, [open]),
+  )
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        gap: '3.5vw',
+      }}
+    >
+      <PlaceFill />
+      <JobContainer
+        sx={{
+          display: isOpen ? 'none' : 'flex',
+        }}
+      >
+        <SearchContainer>
+          <SearchBox
+            state={state}
+            filterData={filterData}
+            setState={setState}
+            labels={'ค้นหางานทั้งหมด'}
+          />
+        </SearchContainer>
+
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+          }}
+        >
+          <InfiniteScroll
+            loadMore={fetchData}
+            hasMore={hasMore}
+            loader={
+              <div
+                className="loader"
+                key={0}
+                style={{
+                  display: 'flex',
+                  alignItems: 'column',
+                  justifyContent: 'center',
+                }}
+              >
+                <CircularProgress color="primary" />
+              </div>
+            }
+          >
+            {job && <JobCardContainer {...job} />}
+          </InfiniteScroll>
+        </div>
+      </JobContainer>
+      <FilterBoxContainer>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: '1rem',
+          }}
+        >
+          <FilterContainer
+            state={state}
+            setState={setState}
+            isTitle={false}
+            filterData={filterData}
+          />
+        </div>
+      </FilterBoxContainer>
+
+      {isOpen && (
+        <FilterMobileContainer
+          state={state}
+          setState={setState}
+          isFilterShow={isOpen}
+          closeFilterPage={close}
+          filterData={filterData}
+        />
+      )}
+    </div>
+  )
 }
 
 export default withGuard(AppliedJobPage, 'verified')
