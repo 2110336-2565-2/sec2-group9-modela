dd9247972a01f239ac74c80b5172003179db7626
Naphat <priasdevo@gmail.com>
2023-03-01 19:32:56 +0700

    refactor: move chips color/label/outline condition to constant file & change to common chips

--- apps/web/src/modules/job/components/JobCardHeader/components/applicationStatusChip/index.tsx
+++ apps/web/src/modules/job/components/JobCardHeader/components/applicationStatusChip/index.tsx
@@ -1,6 +1,10 @@
-import { ApplicationStatus } from '@modela/dtos'
-import { Chip } from '@mui/material'
+import Chip from 'common/components/Chip'
 
+import {
+  APPLICATION_COLOR,
+  APPLICATION_LABEL,
+  APPLICATION_OUTLINE,
+} from './constants'
 import { ApplicationStatusChipProps } from './types'
 
 export default function ApplicationStatusChip(
@@ -9,45 +13,9 @@ export default function ApplicationStatusChip(
   const { applicationStatus } = props
   return (
     <Chip
-      label={
-        applicationStatus === ApplicationStatus.PENDING
-          ? 'รอคัดเลือก'
-          : applicationStatus === ApplicationStatus.OFFER_SENT
-          ? 'ผ่านการคัดเลือก'
-          : applicationStatus === ApplicationStatus.REJECTED
-          ? 'ไม่ผ่านการคัดเลือก'
-          : applicationStatus === ApplicationStatus.OFFER_ACCEPTED
-          ? 'ยอมรับข้อเสนอ'
-          : 'ปฎิเสธข้อเสนอ'
-      }
-      sx={{
-        color:
-          applicationStatus === ApplicationStatus.PENDING
-            ? '#D28A1D'
-            : applicationStatus === ApplicationStatus.OFFER_SENT
-            ? '#66A373'
-            : applicationStatus === ApplicationStatus.REJECTED
-            ? '#AA5B5B'
-            : applicationStatus === ApplicationStatus.OFFER_ACCEPTED
-            ? '#66A373'
-            : '#AA5B5B',
-        background:
-          applicationStatus === ApplicationStatus.PENDING
-            ? 'rgba(210, 138, 29, 0.2)'
-            : applicationStatus === ApplicationStatus.OFFER_SENT
-            ? '#E0ECE3'
-            : applicationStatus === ApplicationStatus.REJECTED
-            ? '#EEDEDE'
-            : applicationStatus === ApplicationStatus.OFFER_ACCEPTED
-            ? '#E0ECE3'
-            : '#EEDEDE',
-        border:
-          applicationStatus === ApplicationStatus.OFFER_REJECTED
-            ? '1px solid currentColor'
-            : applicationStatus === ApplicationStatus.OFFER_ACCEPTED
-            ? '1px solid currentColor'
-            : '',
-      }}
+      label={APPLICATION_LABEL[applicationStatus]}
+      variant={APPLICATION_COLOR[applicationStatus]}
+      outlined={APPLICATION_OUTLINE[applicationStatus]}
     />
   )
 }
