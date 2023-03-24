1cccef6f466157d961de51ce06ea0ec79d952a27
miello <partearth1212@gmail.com>
2023-03-24 22:08:46 +0700

    feat: add report button to actor card

--- apps/web/src/modules/job/appliedActor/components/ActorCardHeader/index.test.tsx
+++ apps/web/src/modules/job/appliedActor/components/ActorCardHeader/index.test.tsx
@@ -1,6 +1,7 @@
-import { ApplicationStatus } from '@modela/database'
+import { ApplicationStatus, JobStatus } from '@modela/dtos'
 import { render } from '@testing-library/react'
-import { mockAndSpy } from 'common/utils/testing'
+import { mockAndSpy, mockAndSpyMany } from 'common/utils/testing'
+import { mockRouter } from 'common/utils/testing/mockRouter'
 import React from 'react'
 
 describe('<ActorCardHeader />', () => {
@@ -15,6 +16,10 @@ describe('<ActorCardHeader />', () => {
 
   const ProfileImageSpy = mockAndSpy('common/components/ProfileImage')
   const ChipSpy = mockAndSpy('common/components/Chip')
+  const ReportOutlinedSpy = mockAndSpy('@mui/icons-material/ReportOutlined')
+  const [IconButtonSpy] = mockAndSpyMany('@mui/material', ['IconButton'])
+
+  mockRouter(true, { jobId: 1 })
 
   const { default: ActorCardHeader } = require('.') as typeof import('.')
 
@@ -22,14 +27,32 @@ describe('<ActorCardHeader />', () => {
     jest.clearAllMocks()
   })
 
-  describe('normal behavior', () => {
-    describe('display', () => {
-      it('should render ActorCardHeader correctly', () => {
-        render(<ActorCardHeader {...MOCK_PROPS} />)
+  describe('job is not SELECTION_END', () => {
+    it('should render ActorCardHeader correctly', () => {
+      render(<ActorCardHeader jobStatus={JobStatus.OPEN} {...MOCK_PROPS} />)
 
-        expect(ProfileImageSpy).toBeCalledTimes(1)
-        expect(ChipSpy).toBeCalledTimes(1)
-      })
+      expect(ProfileImageSpy).toBeCalledTimes(1)
+      expect(ChipSpy).toBeCalledTimes(1)
+    })
+  })
+
+  describe('job is SELECTION_END', () => {
+    it('should render ActorCardHeader correctly', () => {
+      render(
+        <ActorCardHeader
+          jobStatus={JobStatus.SELECTION_ENDED}
+          {...MOCK_PROPS}
+        />,
+      )
+
+      expect(ProfileImageSpy).toBeCalledTimes(1)
+      expect(IconButtonSpy).toBeCalledWith(
+        expect.objectContaining({
+          href: '/job/1/actor/1/refund',
+        }),
+      )
+      expect(ChipSpy).toBeCalledTimes(0)
+      expect(ReportOutlinedSpy).toBeCalledTimes(1)
     })
   })
 })
---
+++ apps/web/src/modules/job/appliedActor/components/ActorCardHeader/types.ts
@@ -0,0 +1,6 @@
+import { ActorDto, JobStatus } from '@modela/dtos'
+
+export interface ActorCardHeaderProps
+  extends Omit<ActorDto, 'resumeUrl' | 'applicationId'> {
+  jobStatus: JobStatus
+}
