5561c708a886385a99352fb52b2bf0a5651730e1
BadNameOk <jjpocharatf2@gmail.com>
2023-03-25 21:00:40 +0700

    feat: implemented accept-reject job offer

--- apps/api/src/modules/job/application/offer/offer.service.ts
+++ apps/api/src/modules/job/application/offer/offer.service.ts
@@ -38,4 +38,48 @@ export class OfferService {
     )
     return { message: 'Job offer sent' }
   }
+
+  async acceptJobOffer(jobId: number, actorId: number) {
+    const job = await this.jobRepository.getBaseJobById(jobId)
+    if (!job) {
+      throw new NotFoundException('Job not found')
+    }
+    const application =
+      await this.applicationRepository.getApplicationbyActorJob(actorId, jobId)
+    if (!application) {
+      throw new BadRequestException('You have not applied to this job')
+    }
+    if (application.status === ApplicationStatus.OFFER_ACCEPTED) {
+      throw new BadRequestException('You have already accepted this job offer')
+    }
+    if (application.status !== ApplicationStatus.OFFER_SENT) {
+      throw new BadRequestException('You cannot accept this job offer')
+    }
+    await this.applicationRepository.updateApplicationStatus(
+      application.applicationId,
+      ApplicationStatus.OFFER_ACCEPTED,
+    )
+  }
+
+  async rejectJobOffer(jobId: number, actorId: number) {
+    const job = await this.jobRepository.getBaseJobById(jobId)
+    if (!job) {
+      throw new NotFoundException('Job not found')
+    }
+    const application =
+      await this.applicationRepository.getApplicationbyActorJob(actorId, jobId)
+    if (!application) {
+      throw new BadRequestException('You have not applied to this job')
+    }
+    if (application.status === ApplicationStatus.OFFER_REJECTED) {
+      throw new BadRequestException('You have already rejected this job offer')
+    }
+    if (application.status !== ApplicationStatus.OFFER_SENT) {
+      throw new BadRequestException('You cannot reject this job offer')
+    }
+    await this.applicationRepository.updateApplicationStatus(
+      application.applicationId,
+      ApplicationStatus.OFFER_REJECTED,
+    )
+  }
 }
