a1c69c97fc236ad1261ac19c1aea93b0c68fda65
SuitedIceZ <okshopm575@gmail.com>
2023-03-26 20:59:05 +0700

    test: add dependency of testing refund

--- apps/api/src/modules/refund/refund.service.spec.ts
+++ apps/api/src/modules/refund/refund.service.spec.ts
@@ -1,21 +1,41 @@
-//import { mock } from '@modela/database'
 import { Test, TestingModule } from '@nestjs/testing'
 import { PrismaService } from 'src/database/prisma.service'
 
+import { ApplicationRepository } from '../job/application/application.repository'
+import { JobRepository } from '../job/job.repository'
+import { NotificationModule } from '../notification/notification.module'
 import { RefundRepository } from './refund.repository'
 import { RefundService } from './refund.service'
 
 describe('RefundService', () => {
   let service: RefundService
-  //let repository: RefundRepository
-
+  /*
+  let repository: RefundRepository
+  let jobRepository: JobRepository
+  let applicationRepository: ApplicationRepository
+  let notificationService: NotificationService
+  */
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [RefundService, RefundRepository, PrismaService],
+      providers: [
+        RefundService,
+        RefundRepository,
+        PrismaService,
+        JobRepository,
+        ApplicationRepository,
+      ],
+      imports: [NotificationModule],
     }).compile()
 
     service = module.get<RefundService>(RefundService)
-    //repository = module.get<RefundRepository>(RefundRepository)
+    /*
+    repository = module.get<RefundRepository>(RefundRepository)
+    jobRepository = module.get<JobRepository>(JobRepository)
+    applicationRepository = module.get<ApplicationRepository>(
+      ApplicationRepository,
+    )
+    notificationService = module.get<NotificationService>(NotificationService)
+    */
   })
 
   it('should be defined', () => {
