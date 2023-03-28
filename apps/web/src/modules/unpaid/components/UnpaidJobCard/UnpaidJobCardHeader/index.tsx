640cbfd3ef0a8a255b656a8ee84d3a17d21723e0
CDGstudent <gearforcoding@gmail.com>
2023-03-28 13:09:14 +0700

    feat: add UnpaidJobCard

---
+++ apps/web/src/modules/unpaid/components/UnpaidJobCard/UnpaidJobCardHeader/index.tsx
@@ -0,0 +1,46 @@
+import { Typography } from '@mui/material'
+import ProfileImage from 'common/components/ProfileImage'
+import Link from 'next/link'
+import React from 'react'
+
+import { HeaderRow, TitleContainer } from './styled'
+import { HeaderProps } from './types'
+
+const UnpaidJobCardHeader = (prop: HeaderProps) => {
+  const {
+    jobCastingImageUrl,
+    companyName,
+    title,
+    isDetail,
+    castingId,
+    castingName,
+  } = prop
+
+  return (
+    <HeaderRow>
+      <Link
+        href={`/profile/${castingId}`}
+        passHref
+        style={{ textDecoration: 'none', color: 'inherit' }}
+      >
+        <ProfileImage
+          src={jobCastingImageUrl}
+          firstName={castingName}
+          userId={castingId}
+          sx={{ marginTop: '4px' }}
+        />
+      </Link>
+      <TitleContainer>
+        <Typography variant="h6" sx={{ wordBreak: 'break-word' }}>
+          {!isDetail &&
+            `${title.substring(0, 50)}${title.length >= 50 ? '...' : ''}`}
+          {isDetail && title}
+        </Typography>
+        <Typography fontWeight={400} sx={{ wordBreak: 'break-word' }}>
+          {companyName}
+        </Typography>
+      </TitleContainer>
+    </HeaderRow>
+  )
+}
+export default UnpaidJobCardHeader
---
+++ apps/web/src/modules/unpaid/components/UnpaidJobCard/styled.ts
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material'
+
+export const CardContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 100%;
+  background-color: white;
+  border: 1px solid rgba(0, 0, 0, 0.12);
+  border-radius: 12px;
+  padding: 12px;
+  cursor: pointer;
+`
