dc5cf3625b95b51e85591e9f8f7329f5d68513b9
SuitedIceZ <okshopm575@gmail.com>
2023-03-28 11:17:05 +0700

    feat: filter out refunded job in actor history

--- apps/api/src/modules/user/user.repository.ts
+++ apps/api/src/modules/user/user.repository.ts
@@ -3,6 +3,7 @@ import {
   Casting,
   JobStatus,
   Prisma,
+  RefundStatus,
   User,
   UserStatus,
 } from '@modela/database'
@@ -135,24 +136,38 @@ export class UserRepository {
             User: true,
           },
         },
+        Application: {
+          where: {
+            actorId: paramId,
+            status: ApplicationStatus.OFFER_ACCEPTED,
+          },
+          include: {
+            Refund: true,
+          },
+        },
       },
     })
 
-    const returnJobs = jobs.map((job) => ({
-      jobId: job.jobId,
-      title: job.title,
-      companyName: job.Casting.companyName,
-      description: job.description,
-      status: job.status,
-      actorCount: job.actorCount,
-      gender: job.gender,
-      wage: job.wage,
-      applicationDeadline: job.applicationDeadline,
-      jobCastingImageUrl: job.Casting.User.profileImageUrl,
-      castingId: job.castingId,
-      castingName: job.Casting.User.firstName,
-    }))
-
+    let returnJobs = jobs.map(
+      // assume that there is only one filtered application for each job
+      (job) =>
+        job.Application[0].Refund.refundStatus !== RefundStatus.ACCEPTED && {
+          jobId: job.jobId,
+          title: job.title,
+          companyName: job.Casting.companyName,
+          description: job.description,
+          status: job.status,
+          actorCount: job.actorCount,
+          gender: job.gender,
+          wage: job.wage,
+          applicationDeadline: job.applicationDeadline,
+          jobCastingImageUrl: job.Casting.User.profileImageUrl,
+          castingId: job.castingId,
+          castingName: job.Casting.User.firstName,
+        },
+    )
+    // remove undefined
+    returnJobs = returnJobs.filter((job) => job)
     return returnJobs
   }
 
